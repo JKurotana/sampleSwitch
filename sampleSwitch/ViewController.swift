5eccd77690e13f73395051f045a6aeb7ccf54d91
JUN TANAKA <eijunkyo6037@gmail.com>
2017-03-08 15:11:16 +0800

    init commit

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -6,15 +6,35 @@
 //  Copyright © 2017年 JUN TANAKA. All rights reserved.
 //
 
-import UIKit
+import UIKit //フレームワーク。これがないと、画面上の部品が使えない
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    // 画面が起動した時1回実行されるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // スイッチの状態が変わった時発動
+    @IBAction func changeSwitch(_ sender: UISwitch) {
+        
+        print(sender.isOn)
+        
+        if sender.isOn {
+            // SwitchがONの時実行される
+            print("スイッチオン")
+        }else{
+            // SwitchがOFFの時実行される
+            print("スイッチオフ")
+        }
+    }
+    
 
+    // メモリ不足を感知した時に実行されるメソッド
+    // (ほとんど実行されない)
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
